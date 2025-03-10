4d8cfc13acb62ca4a561865f6396cefdd10f7c22
Pramod <pramodkumar.damam73@gmail.com>
2025-03-10 23:07:08 +0530

    Add thread-cleanup script

--- server/src/cli.ts
+++ server/src/cli.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { prisma } from "libs/prisma";
+import { cleanupThreads } from "./scripts/thread-cleanup";
 
 async function main() {
   const scrapes = await prisma.scrape.findMany();
@@ -10,4 +11,5 @@ async function main() {
 }
 
 console.log("Starting...");
-main();
+// main();
+cleanupThreads();
---
+++ server/src/scripts/thread-cleanup.ts
@@ -0,0 +1,33 @@
+import { prisma } from "libs/prisma";
+
+function chunk<T>(array: T[], size: number): T[][] {
+  return array.reduce((acc, _, i) => {
+    const index = Math.floor(i / size);
+    acc[index] = [...(acc[index] || []), array[i]];
+    return acc;
+  }, [] as T[][]);
+}
+
+export async function cleanupThreads() {
+  const oneWeekAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7);
+  let threads = await prisma.thread.findMany({
+    where: {
+      createdAt: {
+        lt: oneWeekAgo,
+      },
+    },
+  });
+
+  threads = threads.filter((thread) => thread.messages.length > 0);
+  console.log("Found", threads.length, "threads");
+
+  const chunks = chunk(threads, 100);
+  for (const chunk of chunks) {
+    console.log("Deleting chunk", chunk.length, "threads");
+    await prisma.thread.deleteMany({
+      where: {
+        id: { in: chunk.map((thread) => thread.id) },
+      },
+    });
+  }
+}
