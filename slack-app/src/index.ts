13eb0faaad2e664d8852d213754e0f601a0384e4
Pramod <pramodkumar.damam73@gmail.com>
2025-04-10 16:33:01 +0530

    Integrate api

--- slack-app/src/index.ts
+++ slack-app/src/index.ts
@@ -4,6 +4,8 @@ dotenv.config();
 import { App } from "@slack/bolt";
 import { InstallationStore } from "@slack/oauth";
 import { prisma } from "libs/prisma";
+import { createToken } from "./jwt";
+import { query } from "./api";
 
 const installationStore: InstallationStore = {
   storeInstallation: async (installation) => {
@@ -83,18 +85,59 @@ const app = new App({
   },
 });
 
-app.message("hello", async ({ message, say, client, context }) => {
-  // const history = await client.conversations.history({
-  //   channel: message.channel,
-  //   limit: 15,
-  // });
+function cleanText(text: string) {
+  return text.replace(/<@[^>]+>/g, "").trim();
+}
 
-  console.log({ context });
+app.message(
+  `<@${process.env.SLACK_USER_ID}>`,
+  async ({ message, say, client, context }) => {
+    const history = await client.conversations.history({
+      channel: message.channel,
+      limit: 15,
+    });
 
-  await say({
-    text: `Hey there <@${(message as any).user}>!`,
-  });
-});
+    if (!history.messages) {
+      await say({
+        text: "No messages found",
+      });
+      return;
+    }
+
+    const llmMessages = history.messages.map((m) => ({
+      role: m.user === process.env.SLACK_USER_ID ? "assistant" : "user",
+      content: cleanText(m.text ?? ""),
+    }));
+
+    const scrape = await prisma.scrape.findFirst({
+      where: {
+        slackTeamId: context.teamId,
+      },
+    });
+
+    if (!scrape) {
+      await say({
+        text: "You need to integrate your Slack with CrawlChat.app first!",
+      });
+      return;
+    }
+
+    const { answer, json, error } = await query(
+      scrape.id,
+      llmMessages,
+      createToken(scrape.userId),
+      {
+        prompt: "This would be a Slack message. Use only code blocks, lists from markdown. Don't use headings, tables, links and other blocks from markdown",
+      }
+    );
+
+    await say({
+      text: answer,
+      mrkdwn: true,
+      thread_ts: message.ts,
+    });
+  }
+);
 
 (async () => {
   // Start your app
