7bda92354f67891743792d2cf5f43a61023a6434
Pramod <pramodkumar.damam73@gmail.com>
2025-04-20 00:03:38 +0530

    Empty state

--- front/app/conversations.tsx
+++ front/app/conversations.tsx
@@ -1,4 +1,13 @@
-import { Badge, Center, Flex, Group, Stack, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  Center,
+  EmptyState,
+  Flex,
+  Group,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { Page } from "./components/page";
 import { TbMessage } from "react-icons/tb";
 import type { Route } from "./+types/conversations";
@@ -25,7 +34,7 @@ export async function loader({ request }: Route.LoaderArgs) {
   const messages = await prisma.message.findMany({
     where: {
       ownerUserId: user!.id,
-      // scrapeId,
+      scrapeId,
       createdAt: {
         gte: ONE_WEEK_AGO,
       },
@@ -68,66 +77,89 @@ export default function Conversations({ loaderData }: Route.ComponentProps) {
 
   return (
     <Page title="Conversations" icon={<TbMessage />} noPadding>
-      <Flex h="full">
-        <Stack
-          maxW="300px"
-          w="full"
-          borderRight={"1px solid"}
-          borderColor="brand.outline"
-          h="full"
-          maxH={"calc(100dvh - 60px)"}
-          gap={0}
-          overflowY={"auto"}
-        >
-          {loaderData.threads.map((thread) => (
-            <Stack
-              key={thread.id}
-              borderBottom={"1px solid"}
-              borderColor="brand.outline"
-              px={4}
-              py={2}
-              gap={1}
-              cursor={"pointer"}
-              bg={
-                selectedThread?.id === thread.id ? "brand.gray.50" : undefined
-              }
-              _hover={{ bg: "brand.gray.50" }}
-              onClick={() => setSelectedThread(thread)}
-            >
-              <Group justifyContent={"space-between"}>
-                <Text opacity={0.8}>
-                  {thread.id.substring(thread.id.length - 4)}
+      {loaderData.threads.length === 0 && (
+        <EmptyState.Root>
+          <EmptyState.Content>
+            <EmptyState.Indicator>
+              <TbMessage />
+            </EmptyState.Indicator>
+            <VStack textAlign="center">
+              <EmptyState.Title>No conversations found</EmptyState.Title>
+              <EmptyState.Description>
+                Integrate the collection on your website to start making
+                conversations
+              </EmptyState.Description>
+            </VStack>
+          </EmptyState.Content>
+        </EmptyState.Root>
+      )}
+      {loaderData.threads.length > 0 && (
+        <Flex h="full">
+          <Stack
+            maxW="300px"
+            w="full"
+            borderRight={"1px solid"}
+            borderColor="brand.outline"
+            h="full"
+            maxH={"calc(100dvh - 60px)"}
+            gap={0}
+            overflowY={"auto"}
+          >
+            <Text p={4} opacity={0.5}>
+              Here are the conversations made by your customers or community on
+              your website
+            </Text>
+            {loaderData.threads.map((thread) => (
+              <Stack
+                key={thread.id}
+                borderBottom={"1px solid"}
+                borderColor="brand.outline"
+                px={4}
+                py={2}
+                gap={1}
+                cursor={"pointer"}
+                bg={
+                  selectedThread?.id === thread.id ? "brand.gray.50" : undefined
+                }
+                _hover={{ bg: "brand.gray.50" }}
+                _first={{ borderTop: "1px solid", borderColor: "brand.outline" }}
+                onClick={() => setSelectedThread(thread)}
+              >
+                <Group justifyContent={"space-between"}>
+                  <Text opacity={0.8}>
+                    {thread.id.substring(thread.id.length - 4)}
+                  </Text>
+                  <Badge colorPalette={"brand"} variant={"surface"}>
+                    {thread.messages.length}
+                  </Badge>
+                </Group>
+                <Text opacity={0.5} fontSize={"sm"}>
+                  {moment(thread.createdAt).fromNow()}
                 </Text>
-                <Badge colorPalette={"brand"} variant={"surface"}>
-                  {thread.messages.length}
-                </Badge>
-              </Group>
-              <Text opacity={0.5} fontSize={"sm"}>
-                {moment(thread.createdAt).fromNow()}
-              </Text>
-            </Stack>
-          ))}
-        </Stack>
-        <Stack h="full" flex={1} bg="brand.gray.100">
-          <Center h="full" w="full">
-            {selectedThread && (
-              <ChatBox
-                thread={selectedThread}
-                scrape={loaderData.scrape!}
-                userToken={"NA"}
-                key={selectedThread!.id}
-                onBgClick={() => {}}
-                onPin={() => {}}
-                onUnpin={() => {}}
-                onErase={() => {}}
-                onDelete={() => {}}
-                messages={selectedThread.messages}
-                showScore
-              />
-            )}
-          </Center>
-        </Stack>
-      </Flex>
+              </Stack>
+            ))}
+          </Stack>
+          <Stack h="full" flex={1} bg="brand.gray.100">
+            <Center h="full" w="full">
+              {selectedThread && (
+                <ChatBox
+                  thread={selectedThread}
+                  scrape={loaderData.scrape!}
+                  userToken={"NA"}
+                  key={selectedThread!.id}
+                  onBgClick={() => {}}
+                  onPin={() => {}}
+                  onUnpin={() => {}}
+                  onErase={() => {}}
+                  onDelete={() => {}}
+                  messages={selectedThread.messages}
+                  showScore
+                />
+              )}
+            </Center>
+          </Stack>
+        </Flex>
+      )}
     </Page>
   );
 }
