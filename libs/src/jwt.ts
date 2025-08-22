0e6196c0b83763f45afaf36225fb18db73dc907a
Pramod <pramodkumar.damam73@gmail.com>
2025-08-22 20:40:34 +0530

    Cleanup jwt

--- libs/src/jwt.ts
+++ libs/src/jwt.ts
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 import type { JwtPayload } from "jsonwebtoken";
-import { prisma, ScrapeUser } from "./prisma";
 
 interface UserPayload extends JwtPayload {
   userId: string;
@@ -18,34 +17,4 @@ export function createToken(
   return jwt.sign({ userId }, process.env.JWT_SECRET!, {
     expiresIn: `${expiresInSeconds}s`,
   });
-}
-
-export function authoriseScrapeUser(
-  scrapeUsers: ScrapeUser[],
-  scrapeId: string
-) {
-  if (!scrapeUsers.find((su) => su.scrapeId === scrapeId)) {
-    throw new Error("Unauthorised");
-  }
-}
-
-export async function getJwtAuthUser(token?: string | null) {
-  if (!token) {
-    throw Response.json({ error: "No token provided" }, { status: 401 });
-  }
-
-  const userPayload = jwt.verify(token, process.env.JWT_SECRET!) as UserPayload;
-
-  const user = await prisma.user.findUnique({
-    where: { id: userPayload.userId },
-    include: {
-      scrapeUsers: true,
-    },
-  });
-
-  if (!user) {
-    throw Response.json({ error: "Invalid token" }, { status: 401 });
-  }
-
-  return user;
 }
