02caa85c27aa792e88662e2c998c5ea929914e74
Ayushman <tiwariayushman847@gmail.com>
2026-02-16 22:02:42 +0530

    Chrome extension context menus (#514)
    * #510
    
    * Fix in Chrome extension
    
    * Swap fill and compose & simplify
    
    * Update docs and add changelog
    
    ---------
    
    Co-authored-by: Pramod <pramodkumar.damam73@gmail.com>

--- chrome-extension/src/content.ts
+++ chrome-extension/src/content.ts
@@ -172,6 +172,7 @@ async function openPanel(
   options?: {
     submit?: boolean;
     autoUse?: boolean;
+    isPrompt?: boolean;
   }
 ) {
   const existingPanel = document.getElementById("crawlchat-panel");
@@ -257,6 +258,7 @@ async function openPanel(
       onFocus: handleFocusElement,
       submit: options?.submit,
       autoUse: options?.autoUse,
+      isPrompt: options?.isPrompt,
     })
   );
 }
@@ -313,11 +315,11 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   }
 
   if (message.type === "OPEN_MODAL_FROM_SHORTCUT") {
-    handleShortcutOpenPanel();
+    handleShortcutOpenPanel(message.isPrompt);
   }
 
   if (message.type === "OPEN_MODAL_AUTO_USE_FROM_SHORTCUT") {
-    handleShortcutOpenPanelAutoUse();
+    handleShortcutOpenPanelAutoUse(message.isPrompt);
   }
 
   if (message.type === "CONFIG_UPDATED") {
@@ -333,7 +335,7 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   }
 });
 
-async function handleShortcutOpenPanel() {
+async function handleShortcutOpenPanel(isPrompt?: boolean) {
   const config = await getConfig();
 
   if (config && config.apiKey && config.scrapeId) {
@@ -344,11 +346,14 @@ async function handleShortcutOpenPanel() {
         | HTMLTextAreaElement
         | HTMLElement);
 
-    openPanel(config, targetElement, { submit: true });
+    openPanel(config, targetElement, {
+      submit: true,
+      isPrompt,
+    });
   }
 }
 
-async function handleShortcutOpenPanelAutoUse() {
+async function handleShortcutOpenPanelAutoUse(isPrompt?: boolean) {
   const config = await getConfig();
 
   if (config && config.apiKey && config.scrapeId) {
@@ -359,6 +364,10 @@ async function handleShortcutOpenPanelAutoUse() {
         | HTMLTextAreaElement
         | HTMLElement);
 
-    openPanel(config, targetElement, { submit: true, autoUse: true });
+    openPanel(config, targetElement, {
+      submit: true,
+      autoUse: true,
+      isPrompt,
+    });
   }
 }
