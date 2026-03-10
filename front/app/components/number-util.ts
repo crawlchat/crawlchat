67b04449852d5959916205d8cbe34a843f8bc148
Pramod Kumar <pramodkumar.damam73@gmail.com>
2026-03-10 13:14:39 +0530

    toFixed in number-utils and fix time saved stat (#625)

--- front/app/components/number-util.ts
+++ front/app/components/number-util.ts
@@ -1,7 +1,7 @@
-export function numberToKMB(num: number) {
+export function numberToKMB(num: number, toFixed?: number) {
   const prefix = num < 0 ? "-" : "";
   num = Math.abs(num);
-  if (num < 1000) return `${prefix}${num}`;
+  if (num < 1000) return `${prefix}${toFixed ? num.toFixed(toFixed) : num}`;
   if (num < 1000000) return `${prefix}${(num / 1000).toFixed(1)}k`;
   return `${prefix}${(num / 1000000).toFixed(1)}M`;
 }
--- front/app/summary/stat-card.tsx
+++ front/app/summary/stat-card.tsx
@@ -7,12 +7,14 @@ export default function StatCard({
   icon,
   suffix,
   tooltip,
+  toFixed,
 }: {
   label: string;
   value?: number;
   icon: React.ReactNode;
   suffix?: string;
   tooltip?: string;
+  toFixed?: number;
 }) {
   return (
     <div className="tooltip tooltip-bottom" data-tip={tooltip}>
@@ -26,7 +28,7 @@ export default function StatCard({
           <div className="stat-figure text-3xl opacity-50">{icon}</div>
           <div className="stat-title">{label}</div>
           <div className="stat-value">
-            {value !== undefined ? numberToKMB(value) : ""}
+            {value !== undefined ? numberToKMB(value, toFixed) : ""}
             {suffix}
           </div>
         </div>
